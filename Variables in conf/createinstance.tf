95d741afd8c3f0fe3e2223363afaae614594a2a7
jaga <jagadce@gmail.com>
2022-02-20 17:32:29 -0600

    m

--- Variables in conf/createinstance.tf
+++ Variables in conf/createinstance.tf
@@ -3,4 +3,5 @@ resource "aws_instance" "Test" {
 ami = "ami-05803413c51f242b7"
 instance_type = "t2.micro"
 tags = {Name ="Test-${count.index}"}
+security_groups = "{var.security_groups}"
 }
--- Variables in conf/variables.tf
+++ Variables in conf/variables.tf
@@ -3,3 +3,8 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
 default = "us-east-2"  
 }
+variable "security_groups" {
+    type = "list"
+    default = ["sg-1234","sg-56678","sg-8798"]
+ 
+}
