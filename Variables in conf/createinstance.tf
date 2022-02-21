ed2767a638199d2c7849db52bbf7c805463722cc
jaga <jagadce@gmail.com>
2022-02-20 18:40:24 -0600

    m

--- Variables in conf/createinstance.tf
+++ Variables in conf/createinstance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "Test" {
     count = 3
-ami = lookup (var.AWS_REGION, var.ami)
+ami = lookup (var.ami,var.AWS_REGION)
 instance_type = "t2.micro"
 tags = {Name ="Test-${count.index}"}
 security_groups = "${var.security_groups}"
