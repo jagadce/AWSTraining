6e8ca59686a01169cc88afa8c2e3c4ba673acc7d
jaga <jagadce@gmail.com>
2022-02-21 18:45:47 -0600

    m

--- DataSource/createinstance.tf
+++ DataSource/createinstance.tf
@@ -1,20 +1,16 @@
 data "aws_availability_zones" "available" {} 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "RHEL" {
     most_recent = true
-    owners = ["099720109477"]
+    owners = ["309956199498"]
     filter {
         name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-xemial-16.04-amd64-server-*"]
+        values = ["RHEL-8*"]
     }
-    filter{
-        name = "vitualization-type"
-        values = ["hvm"] 
-    }
-  
+   
 }
 resource "aws_instance" "Test" {
     count = 3
-ami = data.aws_ami.ubuntu.id
+ami = data.aws_ami.RHEL.image_id
 instance_type = "t2.micro"
 tags = {Name ="Test-${count.index}"}
 availability_zone = data.aws_availability_zones.available.names[0]
