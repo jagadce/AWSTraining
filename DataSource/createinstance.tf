6964fbd79fc841f3d24a785adf7a712ee29ae8ff
jaga <jagadce@gmail.com>
2022-02-22 17:42:24 -0600

    m

--- DataSource/createinstance.tf
+++ DataSource/createinstance.tf
@@ -8,10 +8,10 @@ data "aws_ami" "RHEL" {
     }
    
 }
-resource "aws_instance" "name" {
+resource "aws_instance" "J" {
     count = 3
 ami = data.aws_ami.RHEL.image_id
 instance_type = "t2.micro"
-tags = {Name ="name-${count.index}"}
+tags = {Name ="J-${count.index}"}
 availability_zone = data.aws_availability_zones.available.names[0]
 }
