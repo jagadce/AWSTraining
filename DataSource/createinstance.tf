c83233d22ad36c85776d528078b696c6e20f5b9b
jaga <jagadce@gmail.com>
2022-02-21 18:31:17 -0600

    m

--- DataSource/createinstance.tf
+++ DataSource/createinstance.tf
@@ -14,7 +14,7 @@ data "aws_ami" "ubuntu" {
 }
 resource "aws_instance" "Test" {
     count = 3
-ami = lookup (var.ami,var.AWS_REGION)
+ami = data.aws_ami.ubuntu.id
 instance_type = "t2.micro"
 tags = {Name ="Test-${count.index}"}
 availability_zone = data.aws_availability_zones.available.names[0]
