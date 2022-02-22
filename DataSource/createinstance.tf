709bca7709148855eefb467dd23cfd2d4fd3f8d5
jaga <jagadce@gmail.com>
2022-02-22 17:29:08 -0600

    m

--- DataSource/createinstance.tf
+++ DataSource/createinstance.tf
@@ -1,4 +1,4 @@
-data "aws_availability_zones" "available" {} 
+data "aws_availability_zones" "R" {} 
 data "aws_ami" "RHEL" {
     most_recent = true
     owners = ["309956199498"]
@@ -8,10 +8,10 @@ data "aws_ami" "RHEL" {
     }
    
 }
-resource "aws_instance" "name" {
+resource "aws_instance" "R" {
     count = 3
 ami = data.aws_ami.RHEL.image_id
 instance_type = "t2.micro"
-tags = {Name ="name-${count.index}"}
-availability_zone = data.aws_availability_zones.available.names[0]
+tags = {Name ="R-${count.index}"}
+availability_zone = data.aws_availability_zones.available.names[2]
 }
