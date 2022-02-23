eb9f94e4ac835a49954bf7ecaab026a4eff24014
jaga <jagadce@gmail.com>
2022-02-22 19:45:01 -0600

    m

--- DataSource/createinstance.tf
+++ DataSource/createinstance.tf
@@ -1,5 +1,5 @@
 data "aws_availability_zones" "available" {} 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "Ubuntu" {
     most_recent = true
     owners = ["309956199498"]
     filter {
@@ -11,7 +11,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "Name" {
     count = 3
 instance_type = "t2.micro"
-ami = data.aws_ami.ubuntu.image_id
+ami = data.aws_ami.Ubuntu.image_id
 tags = {Name ="Test-${count.index}"}
 availability_zone = data.aws_availability_zones.available.names[0]
 }
