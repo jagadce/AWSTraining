8962402893aef97df63524fdfd91ed2cfd6fdee9
jaga <jagadce@gmail.com>
2022-02-22 19:49:18 -0600

    m

--- DataSource/createinstance.tf
+++ DataSource/createinstance.tf
@@ -1,17 +1,17 @@
 data "aws_availability_zones" "available" {} 
-data "aws_ami" "Ubuntu" {
+#data "aws_ami" "RHEL" {
     most_recent = true
     owners = ["309956199498"]
     filter {
         name = "name"
-        values = ["ubuntu*"]
+        values = ["RHEL*"]
     }
    
 }
 resource "aws_instance" "Name" {
     count = 3
 instance_type = "t2.micro"
-ami = data.aws_ami.Ubuntu.image_id
-tags = {Name ="Test-${count.index}"}
+ami = ami-0454207e5367abf01
+tags = {Name ="test"}
 availability_zone = data.aws_availability_zones.available.names[0]
 }
