ceea7741ceb622ce8170ab0eb2fddf880c61fb56
jaga <jagadce@gmail.com>
2022-02-22 19:50:58 -0600

    m

--- DataSource/createinstance.tf
+++ DataSource/createinstance.tf
@@ -1,17 +1,17 @@
-data "aws_availability_zones" "available" {} 
+#data "aws_availability_zones" "available" {} 
 #data "aws_ami" "RHEL" {
-    most_recent = true
-    owners = ["309956199498"]
-    filter {
-        name = "name"
-        values = ["RHEL*"]
-    }
+#    most_recent = true
+ #   owners = ["309956199498"]
+  #  filter {
+   #     name = "name"
+    #    values = ["RHEL*"]
+    #}
    
-}
+#}
 resource "aws_instance" "Name" {
-    count = 3
 instance_type = "t2.micro"
 ami = ami-0454207e5367abf01
-tags = {Name ="test"}
-availability_zone = data.aws_availability_zones.available.names[0]
+tags = {
+    Name ="test"}
+#availability_zone = data.aws_availability_zones.available.names[0]
 }
