678d399bff9034290cbed9b7b5af1302471942a8
jaga <jagadce@gmail.com>
2022-02-27 11:47:21 -0600

    m

--- DataSource/createinstance.tf
+++ DataSource/createinstance.tf
@@ -15,3 +15,11 @@ tags = {
     Name ="test1"}
 #availability_zone = data.aws_availability_zones.available.names[0]
 }
+
+output "public_ip" {
+    value = aws_instance.Name.public_ip  
+}
+provisioner "local-exec" {
+command = "echo aws_instance.Name.private_ip >> My_IP.txt"
+
+}
