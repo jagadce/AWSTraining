66098678eb19aac9c7e6ea9791a71d5689ff35ba
jaga <jagadce@gmail.com>
2022-02-28 18:46:12 -0600

    m

---
+++ TerraformWithAWS/nat.tf
@@ -0,0 +1,42 @@
+resource "aws_nat_gateway" "NATGateway" {
+  allocation_id = aws_eip.NATGateway
+  subnet_id     = aws_subnet.PublicTrainingsubnet
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.NATGateway]
+}
+
+
+#Outer Machine cannot access my machine
+resource "aws_route_table" "Private" {
+  vpc_id = aws_vpc.Training.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_internet_gateway.NATGateway
+  }
+
+   tags = {
+    Name = "Private"
+  }
+}
+
+resource "aws_route_table_association" "Private-a" {
+  subnet_id      = aws_subnet.PrivateTrainingsubnet.id
+  route_table_id = aws_route_table.Private
+}
+
+resource "aws_route_table_association" "Private-b" {
+  subnet_id      = aws_subnet.PrivateTrainingsubnet1.id
+  route_table_id = aws_route_table.Private
+}
+
+resource "aws_route_table_association" "Private-c" {
+  subnet_id      = aws_subnet.PrivateTrainingsubnet2.id
+   route_table_id = aws_route_table.Private
+}
---
+++ TerraformWithAWS/vpc.tf
@@ -0,0 +1,122 @@
+#Create VPC
+
+resource "aws_vpc" "Training" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Training"
+  }
+}
+
+#Create Private subnet
+
+resource "aws_subnet" "PrivateTrainingsubnet" {
+  vpc_id     = aws_vpc.Training.id
+  cidr_block = "10.0.1.0/24"
+map_public_ip_on_launch = "default"
+availability_zone = "us-west-2"
+
+  tags = {
+    Name = "Trainig"
+  }
+}
+
+resource "aws_subnet" "PrivateTrainingsubnet1" {
+  vpc_id     = aws_vpc.Training.id
+  cidr_block = "10.0.2.0/24"
+map_public_ip_on_launch = "default"
+availability_zone = "us-west-1"
+
+  tags = {
+    Name = "Trainig"
+  }
+}
+
+resource "aws_subnet" "PrivateTrainingsubnet2" {
+  vpc_id     = aws_vpc.Training.id
+  cidr_block = "10.0.3.0/24"
+map_public_ip_on_launch = "default"
+availability_zone = "us-west-3"
+
+  tags = {
+    Name = "Trainig"
+  }
+}
+
+#Create Public subnet
+
+resource "aws_subnet" "PublicTrainingsubnet" {
+  vpc_id     = aws_vpc.Training.id
+  cidr_block = "10.0.4.0/24"
+map_public_ip_on_launch = "true"
+availability_zone = "us-west-2"
+
+  tags = {
+    Name = "Trainig"
+  }
+}
+
+resource "aws_subnet" "PublicTrainingsubnet1" {
+  vpc_id     = aws_vpc.Training.id
+  cidr_block = "10.0.5.0/24"
+map_public_ip_on_launch = "true"
+availability_zone = "us-west-2"
+
+  tags = {
+    Name = "Trainig"
+  }
+}
+
+resource "aws_subnet" "PublicTrainingsubnet2" {
+  vpc_id     = aws_vpc.Training.id
+  cidr_block = "10.0.6.0/24"
+map_public_ip_on_launch = "true"
+availability_zone = "us-west-1"
+
+  tags = {
+    Name = "Trainig"
+  }
+}
+
+#Create Gateway Internet
+
+resource "aws_internet_gateway" "Gateway" {
+  vpc_id = aws_vpc.Training.id
+
+  tags = {
+    Name = "Gateway"
+  }
+}
+
+#Create Routing Table
+
+resource "aws_route_table" "RoutingTable" {
+  vpc_id = aws_vpc.Training.id
+
+  route {
+    cidr_block = "0.0.0.0/24"
+    gateway_id = aws_internet_gateway.Gateway.id
+  }
+
+   tags = {
+    Name = "RoutingTable"
+  }
+}
+
+#Routing Table association
+
+resource "aws_route_table_association" "Public-a" {
+  subnet_id      = aws_subnet.PublicTrainingsubnet.id
+  route_table_id = aws_route_table.RoutingTable
+}
+
+resource "aws_route_table_association" "Public-b" {
+  subnet_id      = aws_subnet.PublicTrainingsubnet1.id
+  route_table_id = aws_route_table.RoutingTable
+}
+
+resource "aws_route_table_association" "Public-c" {
+  subnet_id      = aws_subnet.PublicTrainingsubnet2.id
+  route_table_id = aws_route_table.RoutingTable
+}
