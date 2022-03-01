e1f7801db54c03a364ebb1593207850d64853ace
jaga <jagadce@gmail.com>
2022-02-28 19:19:14 -0600

    m

--- TerraformWithAWS/nat.tf
+++ TerraformWithAWS/nat.tf
@@ -5,14 +5,14 @@ vpc = true
 resource "aws_nat_gateway" "gw" {
   allocation_id = aws_eip.NATGateway
   subnet_id     = aws_subnet.PublicTrainingsubnet
-
+ depends_on = [aws_internet_gateway.gw]
   tags = {
     Name = "gw NAT"
   }
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.gw]
+ 
 }
 
 
