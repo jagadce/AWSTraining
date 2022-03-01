c00f53ece1c849d38ed854ca716f824cfed4c3d3
jaga <jagadce@gmail.com>
2022-02-28 19:28:01 -0600

    m

--- TerraformWithAWS/vpc.tf
+++ TerraformWithAWS/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "Training" {
 resource "aws_subnet" "PrivateTrainingsubnet" {
   vpc_id     = aws_vpc.Training.id
   cidr_block = "10.0.1.0/24"
-map_public_ip_on_launch = "default"
+map_public_ip_on_launch = "false"
 availability_zone = "us-west-2"
 
   tags = {
