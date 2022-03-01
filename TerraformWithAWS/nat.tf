1167cd96ac44117b24cfc774ce7e37e19b5b22ce
jaga <jagadce@gmail.com>
2022-02-28 19:25:25 -0600

    m

--- TerraformWithAWS/nat.tf
+++ TerraformWithAWS/nat.tf
@@ -22,7 +22,7 @@ resource "aws_route_table" "Private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_internet_gateway.gw.id
+    nat_gateway_id = aws_nat_gateway.gw.id
   }
 
    tags = {
