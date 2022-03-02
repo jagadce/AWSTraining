fee34d514ab6e7ee34638e3242647963fb3d0d46
jaga <jagadce@gmail.com>
2022-03-01 18:03:40 -0600

    m

--- TerraformWithAWS/variables.tf
+++ TerraformWithAWS/variables.tf
@@ -5,3 +5,11 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
 default = "us-west-1"  
 }
+
+variable "PATH_TO_PUBLIC_KEY" {
+    default = "Training_Key.pub"
+    }
+
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "Training_Key"
+    }
