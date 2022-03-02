ebf931bfd63ce7679cd36f68bad69685be58704a
jaga <jagadce@gmail.com>
2022-03-01 20:20:08 -0600

    m

--- TerraformWithAWS/vpc.tf
+++ TerraformWithAWS/vpc.tf
@@ -2,6 +2,9 @@
 resource "aws_vpc" "Training" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink = "false"
 
   tags = {
     Name = "Training"
