88b21a80afa573b7f56e9bdb58daf515338cad77
jaga <jagadce@gmail.com>
2022-03-03 13:10:06 -0600

    m

---
+++ EBS/createinstance.tf
@@ -0,0 +1,38 @@
+resource "aws_key_pair" "Training_Key" {
+  key_name   = "Training_Key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+ }
+
+
+resource "aws_instance" "Test" {
+instance_type = "t2.micro"
+ami = "ami-0454207e5367abf01"
+key_name = aws_key_pair.Training_Key.key_name
+tags = {
+    Name ="test1"
+    }
+#availability_zone = data.aws_availability_zones.available.names[0]
+}
+output "public_ip" {
+    value = aws_instance.Name.public_ip  
+}
+
+#EBSVolume Creation
+
+resource "aws_ebs_volume" "ExternalVolume" {
+  availability_zone = "us-west-2a"
+  size              = 40
+  type = "gp2"
+
+  tags = {
+    Name = "ExternalVolume"
+  }
+}
+
+#Attach the EBS volume to Instance
+
+resource "aws_volume_attachment" "ebs_attach" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.ExternalVolume.id
+  instance_id = aws_instance.Test.id
+}
---
+++ EBS/variables.tf
@@ -0,0 +1,15 @@
+variable "AWS_ACCESS_KEY" {
+    default = "AKIAZY542AUL636LXBPF"
+}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+default = "us-west-1"  
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+    default = "Training_Key.pub"
+    }
+
+#variable "PATH_TO_PRIVATE_KEY" {
+ #   default = "Training_Key"
+  #  }
