1165849babf0bcf57b5d5d4a9136d77a24fc2cc4
jaga <jagadce@gmail.com>
2022-03-04 15:04:02 -0600

    m

--- TerraformWithAWS/UserData/Apacheinstall.sh
+++ TerraformWithAWS/UserData/Apacheinstall.sh
@@ -3,4 +3,4 @@ apt-get update
 apt-get install -y apache2
 systemstl start apache2
 systemstl enable apache2
-echo "<h1> Deployed machine via Terraform<h1>" | sudo tee /var/www/html/index.htnl
+echo "<h1> Deployed machine via Terraform<h1>" | sudo tee /var/www/html/index.html
