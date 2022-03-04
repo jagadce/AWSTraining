d5f100712176e55bb96ba4ed6965c5d20041c4b2
jaga <jagadce@gmail.com>
2022-03-04 14:46:50 -0600

    m

---
+++ TerraformWithAWS/UserData/Apacheinstall.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+apt-get update
+apt-get install -y apache2
+systemstl start apache2
+systemstl enable apache2
+echo "<h1> Deployed machine via Terraform<h1>" | sudo tee /var/www/html/index.htnl
---
+++ TerraformWithAWS/UserData/createinstance.tf
@@ -0,0 +1,25 @@
+resource "aws_key_pair" "Training_Key" {
+  key_name   = "Training_Key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+ }
+
+
+resource "aws_instance" "Test" {
+instance_type = "t2.micro"
+ami = "ami-0454207e5367abf01"
+key_name = aws_key_pair.Training_Key.key_name
+tags = {
+    Name ="Test"
+    }
+#availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+user_data = file("Apacheinstall.sh")
+
+output "public_ip" {
+    value = aws_instance.Test.private_ip  
+}
+
+
+
+
