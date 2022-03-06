40463af1b2179350bf94da1db95758d5be54e0b9
jaga <jagadce@gmail.com>
2022-03-06 16:21:59 -0600

    m

--- TerraformWithAWS/IAMRoles/createinstance.tf
+++ TerraformWithAWS/IAMRoles/createinstance.tf
@@ -8,11 +8,12 @@ resource "aws_instance" "Test" {
 instance_type = "t2.micro"
 ami = "ami-0454207e5367abf01"
 key_name = aws_key_pair.Training_Key.key_name
+iam_instance_profile = aws_iam_instance_profile.S3profile.name
 tags = {
     Name ="Test"
     }
 #availability_zone = data.aws_availability_zones.available.names[0]
-aws_iam_instance_profile = aws_iam_instance_profile.S3profile.name
+
 }
 
 output "public_ip" {
