ddfc435cd445316ae239aecbaf44241fa02e9fbd
jaga <jagadce@gmail.com>
2022-03-07 14:08:46 -0600

    m

--- TerraformWithAWS/Autoscaling/autoscaling.tf
+++ TerraformWithAWS/Autoscaling/autoscaling.tf
@@ -1,8 +1,3 @@
-resource "aws_key_pair" "Training_Key" {
-  key_name   = "Training_Key"
-  public_key = file(var.PATH_TO_PUBLIC_KEY)
- }
-
 #Auto scaling Lauch Configuration
 resource "aws_launch_configuration" "Autoscaling" {
   name_prefix   = "Autoscaling"
@@ -10,6 +5,11 @@ resource "aws_launch_configuration" "Autoscaling" {
   instance_type = "t2.micro"
 key_name = aws_key_pair.Training_Key.key_name
 }
+
+resource "aws_key_pair" "Training_Key" {
+  key_name   = "Training_Key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+ }
 
 #Create Autoscaling Group
 resource "aws_autoscaling_group" "Autoscaling_Group" {
