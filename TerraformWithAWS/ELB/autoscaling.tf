783647fe83e5aae0a41b4a6eb8e58598f403b22a
jaga <jagadce@gmail.com>
2022-03-08 15:26:42 -0600

    m

--- TerraformWithAWS/ELB/autoscaling.tf
+++ TerraformWithAWS/ELB/autoscaling.tf
@@ -5,6 +5,7 @@ resource "aws_launch_configuration" "Autoscaling" {
   instance_type = "t2.micro"
   key_name = aws_key_pair.Training_Key.key_name
   security_groups = [aws_security_group.Secgrp_Instance.id]
+  user_data = "#!/bin/bash\napt-get update\napt -get -y install net-tools nginx\nMYIP='ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2}' | cut -d ':' -f2'\necho 'Heloo Team\nThis is my IP:'$MYIP > /var/www.html.index.html"
 }
 
 resource "aws_key_pair" "Training_Key" {
