3b4243cfc58559f0957d898e11345383715b19e7
jaga <jagadce@gmail.com>
2022-03-12 14:55:10 -0600

    m

--- Terraform Modules/Main.tf
+++ Terraform Modules/Main.tf
@@ -4,7 +4,7 @@ module "EC2_Instance" {
  name = "I1"
  ami =  "ami-0454207e5367abf01"
  instance_type = "t2.micro"
-subnet_id = "subnet-0b430336b308018322"
+subnet_id = "subnet-066062b60cb83654c"
 tags = {
     terraform = "true"
     Environment = "PROD"
