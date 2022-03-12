19cf818fa07f574aefb26dd6a748bcf9975b833c
jaga <jagadce@gmail.com>
2022-03-12 14:46:12 -0600

    m

--- Terraform Modules/Main.tf
+++ Terraform Modules/Main.tf
@@ -5,6 +5,7 @@ module "EC2_Instance" {
  ami =  "ami-0454207e5367abf01"
  instance_type = "t2.micro"
 subnet_id = "PublicTrainingsubnet2"
+region = "us-west-1"
 tags = {
     terraform = "true"
     Environment = "PROD"
--- Terraform Modules/Provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    access_key = "AKIAZY542AUL636LXBPF"
-   # key_name = ""
-    region = "us-west-1"
-}
