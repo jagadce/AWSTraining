01b7f70c21844f57717676a5a6dd73a51a6cfea5
jaga <jagadce@gmail.com>
2022-03-12 14:35:29 -0600

    m

---
+++ Terraform Modules/Main.tf
@@ -0,0 +1,14 @@
+module "EC2_Instance" {
+    source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+
+ name = "I1"
+ ami =  "ami-0454207e5367abf01"
+ instance_type = "t2.micro"
+subnet_id = "PublicTrainingsubnet2"
+tags = 
+{
+    terraform = "true"
+    Environment = "PROD"
+
+}
+}
---
+++ Terraform Modules/Provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    access_key = "AKIAZY542AUL636LXBPF"
+    secret_key = ""
+    region = "us-west-1"
+}
