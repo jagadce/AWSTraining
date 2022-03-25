0496134c6308b941c50bc992b47a06be7ee4f867
jaga <jagadce@gmail.com>
2022-03-25 13:18:43 -0600

    m

---
+++ TerraformWithAWS/Create Instance/createinstance.tf
@@ -0,0 +1,20 @@
+resource "aws_key_pair" "Training_Key" {
+  key_name   = "Training_Key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+ }
+
+
+resource "aws_instance" "Name" {
+instance_type = "t2.micro"
+ami = "ami-0454207e5367abf01"
+key_name = aws_key_pair.Training_Key.key_name
+vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+subnet_id = aws_subnet.PublicTrainingsubnet1.id
+tags = {
+    Name ="test1"
+    }
+#availability_zone = data.aws_availability_zones.available.names[0]
+}
+output "public_ip" {
+    value = aws_instance.Name.public_ip  
+}
