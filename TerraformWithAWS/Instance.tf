f8b62e4f12690d7363d1242a9f272f64a3248b80
jaga <jagadce@gmail.com>
2022-03-25 17:57:59 -0600

    m

---
+++ TerraformWithAWS/Instance.tf
@@ -0,0 +1,13 @@
+
+provider "aws" {
+resource "aws_instance" "Name" {
+instance_type = "t2.micro"
+ami = "ami-0454207e5367abf01"
+access_key = "AKIAZY542AUL636LXBPF"
+region = "us-west-1"  
+tags = {
+    Name ="test1"
+    }
+}
+}
+ 
