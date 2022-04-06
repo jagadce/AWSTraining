cb159332695ead2075cad60523059e7430b7fd5b
jaga <jagadce@gmail.com>
2022-04-05 23:24:59 -0500

    m

--- TerraformWithAWS/VPC/nat.tf
+++ TerraformWithAWS/VPC/nat.tf
@@ -4,7 +4,7 @@ vpc = true
 
 resource "aws_nat_gateway" "gw" {
   allocation_id = aws_eip.NATGateway.id
-  subnet_id     = aws_subnet.PrivateTrainingsubnet.id
+  subnet_id     = aws_subnet.PublicTrainingsubnet.id
  depends_on = [aws_internet_gateway.Gateway]
 
  tags = {
