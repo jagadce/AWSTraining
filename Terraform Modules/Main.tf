43cabee233410aab3c9cdc62e29e62d589c8d15d
jaga <jagadce@gmail.com>
2022-04-05 23:04:17 -0500

    m

--- Terraform Modules/Main.tf
+++ Terraform Modules/Main.tf
@@ -1,7 +1,6 @@
 module "EC2_Instance" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
-
- name = "I1"
+     name = "I1"
  ami =  "ami-0454207e5367abf01"
  instance_type = "t2.micro"
 subnet_id = "subnet-066062b60cb83654c"
