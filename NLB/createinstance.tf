bbffc6d10369d133ec0ffdaaba594d61da19c6c7
jaga <jagadce@gmail.com>
2022-04-07 16:11:31 -0500

    m

--- NLB/createinstance.tf
+++ NLB/createinstance.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "Name" {
 instance_type = "t2.micro"
 ami = "ami-0454207e5367abf01"
 key_name = aws_key_pair.Training_Key.key_name
-vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+vpc_security_group_ids = [aws_security_group.Secgrp_Instance.id]
 subnet_id = aws_subnet.PublicTrainingsubnet1.id
 user_data = file("Apacheinstall.sh")
 tags = {
