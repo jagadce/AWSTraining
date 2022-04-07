50705a23526b51a45dd2838c33fd18110c31d41d
jaga <jagadce@gmail.com>
2022-04-07 15:49:13 -0500

    NLB

---
+++ NLB/LB.tf
@@ -0,0 +1,101 @@
+resource "aws_lb" "NLB" {
+  name               = "NLB"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = [aws_subnet.PublicTrainingsubnet.id,aws_subnet.PublicTrainingsubnet1]
+  security_groups = [aws_security_group.Secgrp_Instance.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}
+   listener {
+    instance_port     = 80
+    instance_protocol = "TCP"
+    lb_port           = 80
+    lb_protocol       = "TCP"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "ELB Work"
+  }
+}
+
+#Securtiy Group for ELB
+
+resource "aws_security_group" "Secgrp_NLB" {
+  name        = "Secgrp_NLB"
+  description = "Secgrp for NLB"
+  vpc_id      = aws_vpc.Training.id
+
+  ingress {
+    description      = "Accpet only 80Port"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+      }
+
+  tags = {
+    Name = "Secgrp_NLB"
+  }
+}
+
+#Securtiy Group for Instance
+
+resource "aws_security_group" "Secgrp_Instance" {
+  name        = "Secgrp_Instance"
+  description = "Secgrp for Instance"
+  vpc_id      = aws_vpc.Training.id
+
+  ingress {
+    description      = "Accpet only ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  
+  }
+ ingress {
+    description      = "Accpet only 80 port from ELB"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups = [aws_security_group.Secgrp_NLB.id]
+  
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+      }
+
+  tags = {
+    Name = "Secgrp_Instance"
+  }
+}
+
---
+++ NLB/nat.tf
@@ -0,0 +1,33 @@
+resource "aws_eip" "NATGateway" {
+vpc = true
+}
+
+resource "aws_nat_gateway" "gw" {
+  allocation_id = aws_eip.NATGateway.id
+  subnet_id     = aws_subnet.PublicTrainingsubnet.id
+ depends_on = [aws_internet_gateway.Gateway]
+
+ tags = {
+    Name = "Test_Nat"
+  }
+}
+
+
+#Outer Machine cannot access my machine
+resource "aws_route_table" "Private" {
+  vpc_id = aws_vpc.Training.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw.id
+  }
+
+   tags = {
+    Name = "Private"
+  }
+}
+
+resource "aws_route_table_association" "Private-a" {
+  subnet_id      = aws_subnet.PrivateTrainingsubnet.id
+  route_table_id = aws_route_table.Private.id
+}
