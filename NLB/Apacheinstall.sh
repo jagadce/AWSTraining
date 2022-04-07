bd83646f4bc5774614733efd6c32a86de3a008e0
jaga <jagadce@gmail.com>
2022-04-07 17:09:26 -0500

    m

--- NLB/Apacheinstall.sh
+++ NLB/Apacheinstall.sh
@@ -3,4 +3,4 @@ apt-get update
 apt-get install -y apache2
 systemstl start apache2
 systemstl enable apache2
-echo "<h1> Deployed machine via Terraform<h1>" | sudo tee /var/www/html/index.html
+echo "<h1> Hello Apace!!!<h1>" | sudo tee /var/www/html/index.html
