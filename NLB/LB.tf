da8e0f166c3b83a73f57efb696f81a80eb1cdc6a
jaga <jagadce@gmail.com>
2022-04-08 13:27:42 -0500

    m

--- NLB/LB.tf
+++ NLB/LB.tf
@@ -46,7 +46,7 @@ resource "aws_lb_target_group" "NLBTargetGroup" {
 #Attaching Instance into Target group
 resource "aws_lb_target_group_attachment" "NLB-Tragetgroup-Attach" {
   target_group_arn = aws_lb_target_group.NLBTargetGroup.arn
-  target_id        = [i-05e263f192255af27, i-0f3982a7b7ecb6688]
+  target_id        = var.target_id
   port             = 80
 }
 
