c4e73c664ee2aa6ce74a3d410353b38e83fc7932
jaga <jagadce@gmail.com>
2022-04-08 14:43:38 -0500

    m

--- NLB/vpc.tf
+++ NLB/vpc.tf
@@ -1,6 +1,6 @@
 #Create VPC
 resource "aws_vpc" "Training" {
-  cidr_block       = "10.0.0.1/24"
+  cidr_block       = "10.0.0.0/24"
   instance_tenancy = "default"
   enable_dns_support = "true"
   enable_dns_hostnames = "true"
