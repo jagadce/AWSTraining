81437814c33d695b20eab4908a5664c26669d292
jaga <jagadce@gmail.com>
2022-04-07 20:13:25 -0500

    m

--- NLB/LB.tf
+++ NLB/LB.tf
@@ -93,8 +93,7 @@ resource "aws_security_group" "Secgrp_Instance" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    security_groups = [aws_security_group.Secgrp_NLB.id]
-  
+    cidr_blocks      = ["0.0.0.0/0"]  
   }
   egress {
     from_port        = 0
