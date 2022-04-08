157f9682725b94b1d91baf987cca7789ac89ddcb
jaga <jagadce@gmail.com>
2022-04-07 21:45:52 -0500

    m

--- NLB/createinstance.tf
+++ NLB/createinstance.tf
@@ -5,6 +5,7 @@ resource "aws_key_pair" "Training_Key" {
 
 
 resource "aws_instance" "test1" {
+    count = 3
 instance_type = "t2.micro"
 ami = "ami-0454207e5367abf01"
 key_name = aws_key_pair.Training_Key.key_name
@@ -12,10 +13,10 @@ vpc_security_group_ids = [aws_security_group.Secgrp_Instance.id]
 subnet_id = aws_subnet.PublicTrainingsubnet1.id
 user_data = file("Apacheinstall.sh")
 tags = {
-    Name ="test1"
+    Name ="MyInstance ${count.index}"
     }
 #availability_zone = data.aws_availability_zones.available.names[0]
 }
-output "public_ip" {
-    value = aws_instance.test1  
-}
+#output "public_ip" {
+#    value = aws_instance.test1  
+#}
