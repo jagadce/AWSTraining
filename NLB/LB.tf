171570fc833dee994ef63332bb75e8e255f980c5
jaga <jagadce@gmail.com>
2022-04-11 15:32:46 -0500

    m

--- NLB/LB.tf
+++ NLB/LB.tf
@@ -15,7 +15,7 @@ resource "aws_lb" "NLB" {
 resource "aws_lb_listener" "NLB_Listener" {
 load_balancer_arn = aws_lb.NLB.arn
   port              = "80"
-  protocol          = "HTTP"
+  protocol          = "TCP"
  #certificate_arn   = "arn:aws:elasticloadbalancing:us-west-1:672021480727:targetgroup/NLBTargetGroup/8df6ba9c0a84b9a0"
   alpn_policy       = "HTTP2Preferred"
 
