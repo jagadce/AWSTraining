f519d76bbe172f41b8ce3a5b4a0ccf12795e4d37
jaga <jagadce@gmail.com>
2022-04-12 13:41:08 -0500

    m

--- NLB/LB.tf
+++ NLB/LB.tf
@@ -6,7 +6,7 @@ resource "aws_lb" "NLB" {
   enable_deletion_protection = true
   #security_groups = [aws_security_group.Secgrp_Instance.id]
   access_logs {
-    bucket        = aws_s3_bucket.nlbprodbucket.id
+    bucket        = aws_s3_bucket_acl.nlbprodbucketacl.acl
     prefix = "prod"
     enabled   = true
   }
