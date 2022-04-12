ff46ba8198f5759e0e20cad7fee4b402213bad3b
jaga <jagadce@gmail.com>
2022-04-12 12:21:24 -0500

    m

--- NLB/LB.tf
+++ NLB/LB.tf
@@ -44,7 +44,7 @@ health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/"
+    port              = 80
     interval            = 30
   }
 }
