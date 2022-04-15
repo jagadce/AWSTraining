c89fada133edeb3bf4dafd8ff501f9d1e4f990ab
jaga <jagadce@gmail.com>
2022-04-15 17:02:48 -0500

    m

--- ALB/LB.tf
+++ ALB/LB.tf
@@ -21,7 +21,7 @@ resource "aws_lb_listener" "ALB_Listener" {
   load_balancer_arn = aws_lb.ALB.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
   #certificate_arn   = ""
 
    default_action {
@@ -57,15 +57,15 @@ resource "aws_lb_listener" "ALB_Listener" {
   #}
 #}
 
-resource "aws_lb_listener_certificate" "listener_certificate" {
-  listener_arn    = aws_lb_listener.ALB_Listener.arn
+#resource "aws_lb_listener_certificate" "listener_certificate" {
+ # listener_arn    = aws_lb_listener.ALB_Listener.arn
   #certificate_arn = aws_acm_certificate.ALB.arn
-  }
+  #}
 
 #Creating Target group 
 resource "aws_lb_target_group" "ALBTargetGroup" {
   name        = "ALBTargetGroup"
-  port        = 80
+  port        = 443
   protocol    = "TCP"
   target_type = "instance"
   vpc_id      = aws_vpc.Training.id
