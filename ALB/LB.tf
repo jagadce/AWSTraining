f0b2913855e3e119edf9552bb1b6d0ffef012c77
jaga <jagadce@gmail.com>
2022-04-17 18:14:03 -0500

    m

--- ALB/LB.tf
+++ ALB/LB.tf
@@ -19,8 +19,8 @@ resource "aws_lb" "ALB" {
   #ALB Listener:
 resource "aws_lb_listener" "ALB_Listener" {
   load_balancer_arn = aws_lb.ALB.arn
-  port              = "443"
-  protocol          = "HTTPS"
+  port              = "80"
+  protocol          = "HTTP"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = "arn:aws:acm:us-west-1:672021480727:certificate/5ec415f0-d3e4-47e9-8b43-d6a5e529f03e"
 
