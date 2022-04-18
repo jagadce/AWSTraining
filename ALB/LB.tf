9f48f499c2babb88126a4b7e332bfd4378d582a3
jaga <jagadce@gmail.com>
2022-04-18 17:34:45 -0500

    m

--- ALB/LB.tf
+++ ALB/LB.tf
@@ -22,7 +22,7 @@ resource "aws_lb_listener" "ALB_Listener" {
   port              = "443"
   protocol          = "HTTPS"
    ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_lb_listener_certificate.listener_certificate.listener_arn
+  certificate_arn   = "arn:aws:acm:us-west-1:672021480727:certificate/73b745ba-45e4-4539-a92f-6a108adec58e"
 
   
    default_action {
@@ -58,10 +58,10 @@ resource "aws_lb_listener" "ALB_Listener" {
   #}
 #}
 
-resource "aws_lb_listener_certificate" "listener_certificate" {
-listener_arn    = aws_lb_listener.ALB_Listener.arn
-certificate_arn = "arn:aws:acm:us-west-1:672021480727:certificate/73b745ba-45e4-4539-a92f-6a108adec58e"
- }
+#resource "aws_lb_listener_certificate" "listener_certificate" {
+#listener_arn    = aws_lb_listener.ALB_Listener.arn
+#certificate_arn = "arn:aws:acm:us-west-1:672021480727:certificate/73b745ba-45e4-4539-a92f-6a108adec58e"
+ #}
 
 #Creating Target group 
 resource "aws_lb_target_group" "ALBTargetGroup" {
