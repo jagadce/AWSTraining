2efc696ad55879377860876ff06579745dbfd872
jaga <jagadce@gmail.com>
2022-04-19 14:13:38 -0500

    m

--- NLB/vpc.tf
+++ NLB/vpc.tf
@@ -60,6 +60,31 @@ availability_zone = "us-west-1a"
   }
 }
 
+#Create Private subnet
+
+resource "aws_subnet" "PrivateTrainingsubnet" {
+  vpc_id     = aws_vpc.Training.id
+  cidr_block = "10.0.1.0/24"
+map_public_ip_on_launch = "false"
+availability_zone = "us-west-1a"
+
+  tags = {
+    Name = "PrivateTrainingsubnet"
+  }
+}
+
+resource "aws_subnet" "PrivateTrainingsubnet1" {
+  vpc_id     = aws_vpc.Training.id
+  cidr_block = "10.0.2.0/24"
+map_public_ip_on_launch = "false"
+availability_zone = "us-west-1a"
+
+  tags = {  
+    Name = "PrivateTrainingsubnet1"
+  }
+}
+
+
 #Create Gateway Internet
 
 resource "aws_internet_gateway" "Gateway" {
