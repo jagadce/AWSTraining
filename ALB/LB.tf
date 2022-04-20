e370d578d5633327e4209d1fd8b757547e8c4555
jaga <jagadce@gmail.com>
2022-04-20 15:19:20 -0500

    m

--- ALB/LB.tf
+++ ALB/LB.tf
@@ -17,7 +17,9 @@ resource "aws_lb" "ALB" {
 resource "aws_lb_listener" "ALB_Listener" {
   load_balancer_arn = aws_lb.ALB.arn
   port              = "80"
-  protocol          = "HTTP"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-west-1:672021480727:certificate/8189bae4-5174-47f8-957d-febb35a671f4"
   
   #default_action {
  # type             = "forward"
@@ -31,9 +33,7 @@ target_group_arn = aws_lb_target_group.ALBTargetGroup.arn
      port        = "443"
       protocol    = "HTTPS"
      status_code = "HTTP_301"
-     ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:acm:us-west-1:672021480727:certificate/8189bae4-5174-47f8-957d-febb35a671f4"
-      }
+        }
    }
 }
  
