da5fad03341674bb080191c7c8277e0fe98d8eee
jaga <jagadce@gmail.com>
2022-04-20 15:24:18 -0500

    m

--- ALB/LB.tf
+++ ALB/LB.tf
@@ -16,6 +16,7 @@ resource "aws_lb" "ALB" {
 #ssl cert generate: openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/apache2/apache2.crt -keyout /etc/apache2/apache2.key
 resource "aws_lb_listener" "ALB_Listener" {
   load_balancer_arn = aws_lb.ALB.arn
+  vpc_security_group_ids = [aws_security_group.Secgrp_ALB.id]
   port              = "80"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
